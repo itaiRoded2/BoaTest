d0ae40048eed4d5acee438fe8248e00faae5d7dc
itaiRoded2 <itai.roded@gmail.com>
2017-08-03 18:31:10 -0700

    Solution to problem of fibonacci

---
+++ BoaTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter current number");
+            Console.WriteLine("Will be used as the given input for current fibonacci stating at 0.");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //Assuming we use the modern usage of Fibonacci  we start at 0 and not 1
+            //https://en.wikipedia.org/wiki/Fibonacci_number
+            Fibonacci(0, 1, 1, number);
+
+            Console.ReadLine();
+
+        }
+
+        //https://www.dotnetperls.com/fibonacci        
+        //Will not work past the 47th num if we use ints (so we use double)- will display minuses see for ref: 
+        //https://www.dotnetperls.com/fibonacci
+        public static void Fibonacci(double current, double next, int counter, int number)
+        {
+            Console.Write(current);
+            if (counter < number)
+            {
+                Console.Write(" ,");
+                Fibonacci(next, current + next, counter + 1, number);
+            }
+            else
+            {
+                if (counter == number)
+                {
+                    Console.Write(".");
+                }
+            }
+                
+        }
+
+    
+    }
+
+}
