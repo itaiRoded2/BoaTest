cdd5a1c6805e5e79add107609c593d99354f8079
itaiRoded2 <itai.roded@gmail.com>
2017-08-04 09:57:34 -0700

    BOA  test bonus question done

--- BoaTest/Program.cs
+++ BoaTest/Program.cs
@@ -14,39 +14,85 @@ namespace BoaTest
             int number = Convert.ToInt32(Console.ReadLine());
 
             //bool to determine we we should display only even indexs - the first number in Fibonacci 0 is considered to be position 1
-            bool dispalyOnlyEvenIndexsInFibonacci = true;
 
-            BoaQuestionSolution(number, dispalyOnlyEvenIndexsInFibonacci);           
+            //Test with True
+            bool shouldDisplayOnlyEvenIndexes = true;
+
+            BoaQuestionSolution(number, shouldDisplayOnlyEvenIndexes);
+
+            Console.WriteLine("\n");
+
+            //Test with False
+            shouldDisplayOnlyEvenIndexes = false;
+
+            BoaQuestionSolution(number, shouldDisplayOnlyEvenIndexes);
 
             Console.ReadLine();
 
         }
 
-        private static void BoaQuestionSolution(int number, bool dispalyOnlyEvenIndexsInFibonacci)
+        private static void BoaQuestionSolution(int number, bool shouldDisplayOnlyEvenIndexes)
         {
             //Assuming we use the modern usage of Fibonacci  we start at 0 and not 1
             //https://en.wikipedia.org/wiki/Fibonacci_number
-            Fibonacci(0, 1, 1, number);
+            Fibonacci(0, 1, 1, number, shouldDisplayOnlyEvenIndexes);
         }
 
         //https://www.dotnetperls.com/fibonacci        
         //Will not work past the 47th num if we use ints (so we use double)- will display minuses see for ref: 
         //https://www.dotnetperls.com/fibonacci
-        public static void Fibonacci(double current, double next, int counter, int number)
+        public static void Fibonacci(double current, double next, int counterIndex, int number, bool shouldDisplayOnlyEvenIndexes)
         {
-            Console.Write(current);
-            if (counter < number)
+            
+
+
+            if (shouldDisplayOnlyEvenIndexes)
             {
-                Console.Write(" ,");
-                Fibonacci(next, current + next, counter + 1, number);
+                if (counterIndex % 2 == 0)
+                {
+                    Console.Write(current);
+
+
+                    if (counterIndex < number && counterIndex < number)
+                    {
+                        Console.Write(", ");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                    
+                }
+
+                if (counterIndex < number)
+                {
+                    
+                    Fibonacci(next, current + next, counterIndex + 1, number, true);
+                }               
+
+
             }
             else
             {
-                if (counter == number)
+                Console.Write(current);
+
+                if (counterIndex < number)
                 {
-                    Console.Write(".");
+                    Console.Write(" ,");
+                    Fibonacci(next, current + next, counterIndex + 1, number, false);
+                }
+                else
+                {
+                    if (counterIndex == number)
+                    {
+                        Console.Write(".");
+                    }
                 }
             }
+
+
+
+         
                 
         }
 
